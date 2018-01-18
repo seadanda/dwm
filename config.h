d33991eda745d9dd65ef8d09685d17b2a85d71ba
Dónal Murray <donal.murray@cern.ch>
2018-01-18 18:46:55 +0000

    Change font to noto sans

--- config.h
+++ config.h
@@ -3,12 +3,12 @@
 #include <X11/XF86keysym.h> // volume keys
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "noto:size=10" };
+static const char dmenufont[]       = "noto:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -56,18 +56,18 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *mutecmd[] = { "amixer", "-q", "sset", "Master", "toggle", NULL };
-static const char *volupcmd[] = { "amixer", "-q", "sset", "PCM", "5-", "unmute", NULL };
-static const char *voldowncmd[] = { "amixer", "-q", "sset", "PCM", "5+", "unmute", NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "gnome-terminal", NULL };
+static char dmenumon[2]         = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *mutecmd[]    = { "pulseaudio-ctl", "mute", NULL };
+static const char *volupcmd[]   = { "pulseaudio-ctl", "up", NULL };
+static const char *voldowncmd[] = { "pulseaudio-ctl", "down", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "gnome-terminal", NULL };
 
 static Key keys[] = {
 	/* modifier            key        function        argument */
 	{ 0,                   XF86XK_AudioMute,    spawn,    {.v = mutecmd } },
-        { 0,                   XF86XK_AudioRaiseVolume,   spawn,    {.v = volupcmd } },
-        { 0,                   XF86XK_AudioLowerVolume,   spawn,    {.v = voldowncmd } },
+    { 0,                   XF86XK_AudioRaiseVolume,   spawn,    {.v = volupcmd } },
+    { 0,                   XF86XK_AudioLowerVolume,   spawn,    {.v = voldowncmd } },
 	{ MODKEY,              XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,    XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,              XK_b,      togglebar,      {0} },
