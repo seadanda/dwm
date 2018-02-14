f62689d1c7640f189ce4bb32bee1b262478e8426
Dónal Murray <donal.murray@cern.ch>
2018-02-14 19:58:32 +0000

    Add brightness control

--- config.h
+++ config.h
@@ -60,14 +60,18 @@ static char dmenumon[2]         = "0"; /* component of dmenucmd, manipulated in 
 static const char *mutecmd[]    = { "pulseaudio-ctl", "mute", NULL };
 static const char *volupcmd[]   = { "pulseaudio-ctl", "up", NULL };
 static const char *voldowncmd[] = { "pulseaudio-ctl", "down", NULL };
+static const char *brightup[]       = { "xbacklight", "-inc", "10", NULL};
+static const char *brightdown[]     = { "xbacklight", "-dec", "10", NULL};
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "gnome-terminal", NULL };
 
 static Key keys[] = {
 	/* modifier            key        function        argument */
-	{ 0,                   XF86XK_AudioMute,    spawn,    {.v = mutecmd } },
+	{ 0,                   XF86XK_AudioMute,          spawn,    {.v = mutecmd } },
     { 0,                   XF86XK_AudioRaiseVolume,   spawn,    {.v = volupcmd } },
     { 0,                   XF86XK_AudioLowerVolume,   spawn,    {.v = voldowncmd } },
+    { 0,                   XF86XK_MonBrightnessUp,    spawn,    {.v = brightup } },
+    { 0,                   XF86XK_MonBrightnessDown,  spawn,    {.v = brightdown } },
 	{ MODKEY,              XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,    XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,              XK_b,      togglebar,      {0} },
