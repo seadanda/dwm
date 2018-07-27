c0ca740d027bb1d02722020cb8249923cde37f6c
Dónal Murray <donal.murray@cern.ch>
2018-07-27 14:19:14 +0100

    Switch to st

--- config.h
+++ config.h
@@ -74,7 +74,7 @@ static const char *dispdiscon[] = { "dispr",                            NULL };
 static const char *brightup[]   = { "xbacklight",       "-inc 10",      NULL };
 static const char *brightdown[] = { "xbacklight",       "-dec 10",      NULL };
 static const char *screenshot[] = { "screenshot",                       NULL };
-static const char *termcmd[]    = { "termite",                          NULL };
+static const char *termcmd[]    = { "st",                               NULL };
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
 static Key keys[] = {
